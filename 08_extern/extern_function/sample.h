ea67548542e45e0dabaeaa61b080bbbee7b2774d
Mikihito Yokoi <interleaved.miki@gmail.com>
2026-07-19 07:01:39 +0900

    Add extern function exercise

---
+++ 08_extern/extern_function/sample.h
@@ -0,0 +1,25 @@
+/*
+【課題25 extern関数】
+
+externを使ってください。
+
+条件
+
+・sample.cに
+    void hello(void);
+  を作成する
+
+・hello()で
+    Hello Extern Function!
+  と表示する
+
+・main.cからhello()を呼び出す
+
+・関数の宣言にはexternを使用する
+
+実行例
+
+Hello Extern Function!
+*/
+
+extern void hello(void);
